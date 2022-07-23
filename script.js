64fad7fa11c4b3063dad2173d628e8b4875800ee
jhoncarter7 <90902071+jhoncarter7@users.noreply.github.com>
2022-07-23 12:09:44 +0530

    extra feature

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let model = document.querySelectorAll(".open-model");
 let container = document.querySelector(".container");
 let blur = document.querySelector(".blur");
 let cancelbtn = document.querySelector(".cancel-btn");
+
 for (let i = 0; i < model.length; i++)
     model[i].addEventListener("click", function () {
 
@@ -10,9 +11,22 @@ for (let i = 0; i < model.length; i++)
         container.classList.remove("hidden")
         blur.classList.remove("hidden")
     })
+
+
+
 //    this is cancel btn to cancel the page 
 
-    cancelbtn.addEventListener("click", ()=>{
+      const closemodel = function () {
         container.classList.add("hidden")
         blur.classList.add("hidden")
-    })
+      }
+
+
+
+
+    //   this is for button to cancel 
+    cancelbtn.addEventListener("click", closemodel);
+
+//    and this is for when we click any were on screen to cancel 
+    blur.addEventListener("click", closemodel);
+
